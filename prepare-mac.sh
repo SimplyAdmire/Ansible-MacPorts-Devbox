d68a52f30c1e043a00c4dd024945f05e9304d3cf
Rens Admiraal <rens@simplyadmire.com>
2015-02-03 17:57:03 +0100

    [TASK] Update prepare-mac script

--- prepare-mac.sh
+++ prepare-mac.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+# This script requires the xcode command line tools to be installed
+if [ ! -f "/etc/paths.d/00-macports" ]; then
+	export PATH=/opt/local/bin:$PATH
+	echo "/opt/local/bin" | sudo tee -a /etc/paths.d/00-macports
+fi
+
 which -s port
 if [[ $? != 0 ]] ; then
 	echo "MacPorts Installation"
 	curl -O https://distfiles.macports.org/MacPorts/MacPorts-2.3.2-10.10-Yosemite.pkg
 	sudo installer -verbose -pkg MacPorts-2.3.2-10.10-Yosemite.pkg -target /
-	sudo port -v selfupdate
+	sudo /opt/local/bin/port -v selfupdate
 	read -p "Continue ? [Enter]"
 	echo ""
 fi
@@ -21,7 +27,7 @@ fi
 which -s ansible
 if [[ $? != 0 ]] ; then
 	echo "Ansible Installation"
-	sudo port install ansible
+	sudo /opt/local/bin/port install ansible
 	read -p "Continue ? [Enter]"
 	echo ""
 fi
